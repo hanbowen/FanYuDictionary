c2b471f6ac00787add7548482cb5837909329275
liubingchuan8@gmail.com <liubingchuan8@gmail.com>
2015-05-28 13:28:17 +0800

    补充User property

--- FanYuDictionary/src/main/java/entity/User.java
+++ FanYuDictionary/src/main/java/entity/User.java
@@ -1,43 +1,59 @@
 package entity;
 
-import java.io.Serializable;  
+import java.util.Map;
 
-import org.springframework.data.annotation.Id;  
    
    
-public class User implements Serializable{  
+public class User extends Entity {  
    
-   
-    private static final long serialVersionUID = 1L;  
+      
+    private String username;  
+    
+    private String password;  
+    
+    private String displayName;
+    
+    private String role;
+    
+    private Map<String, Object> dicSequence;   
        
-    @Id  
-    String uid;  
-    String name;  
-    int age;  
-       
-       
-    public String getUid() {  
-        return uid;  
-    }  
-    public void setUid(String uid) {  
-        this.uid = uid;  
-    }  
-    public String getName() {  
-        return name;  
-    }  
-    public void setName(String name) {  
-        this.name = name;  
-    }  
-    public int getAge() {  
-        return age;  
-    }  
-    public void setAge(int age) {  
-        this.age = age;  
-    }  
-       
-    @Override  
+    public String getUsername() {
+		return username;
+	}
+    
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+	
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	@Override  
     public String toString() {  
-        return "{USER:{uid:"+this.uid+",name:"+this.name+",age:"+this.age+"}}";  
-    }  
+        return "{USER:{id:"+this.id+",username:"+this.username+",displayName:"+this.displayName+",role:" +this.role +"}}";  
+    }
+	
        
 }
