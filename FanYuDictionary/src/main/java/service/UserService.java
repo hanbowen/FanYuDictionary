69386f0a60f337cf0f2d46cfa3ff1d5c52c2897d
liubingchuan8@gmail.com <liubingchuan8@gmail.com>
2015-05-29 11:47:09 +0800

    UserService中调用BaseService中相应的方法

--- FanYuDictionary/src/main/java/service/UserService.java
+++ FanYuDictionary/src/main/java/service/UserService.java
@@ -1,24 +1,84 @@
 package service;
 
-import org.springframework.data.mongodb.core.query.Criteria;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import utils.Pagination;
 import entity.User;
 
 @Service
-public class UserService extends GeneralService{
+public class UserService extends BaseService<User>{
 
 
 	/**
+	 * 存储用户信息
+	 * @param user
+	 * @return
+	 */
+	public User save(User user) {
+		return super.save(user);
+	}
+	
+	/**
+	 * 根据名字查询指定用户
 	 * @param name
 	 * @return
 	 */
 	public User findUserByName(String name) {
-
-		return mongoTemplate.findOne(
-				new Query(Criteria.where("username").is(name)), User.class);
-
+		Map<String , String> criteria = new HashMap<String , String> ();
+		criteria.put("username", name);
+		return super.findOne(criteria);
+	}
+	
+	/**
+	 * 根据ID查询指定用户
+	 * @param name
+	 * @return
+	 */
+	public User findUserById(String id) {
+		Map<String , String> criteria = new HashMap<String , String> ();
+		criteria.put("id", id);
+		return super.findOne(criteria);
+	}
+	
+	/**
+	 * 根据pageNo 及 pageSize 查询每页信息
+	 * @param pageNo
+	 * @param pageSize
+	 * @return
+	 */
+	public Pagination<User> getPageUser(int pageNo, int pageSize) {
+		return super.getPage(pageNo, pageSize);
+	}
+	
+	
+	/**
+	 * 获取总共数据条数
+	 * @return
+	 */
+	public long queryCount() {
+		return super.queryCount(new Query());
+	}
+	
+	/**
+	 * 根据id删除对象
+	 */
+	public void removeById(String id) {
+		super.removeById(id);
+	}
+	
+	/**
+	 * 根据ID更新对象
+	 */
+	public void updateById(String id , Map<String , Object> params) {
+		super.updateById(id, params);
 	}
 
+	@Override
+	protected Class<User> getEntityClass() {
+		return User.class;
+	}
 }
